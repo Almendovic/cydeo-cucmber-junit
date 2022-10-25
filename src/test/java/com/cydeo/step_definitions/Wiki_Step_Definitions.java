898500109602925d1c47799f8cb83631cc894994
Alen <almendovic@gmail.com>
2022-10-25 15:24:30 -0600

    alen df

---
+++ src/test/java/com/cydeo/step_definitions/Wiki_Step_Definitions.java
@@ -0,0 +1,15 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.pages.WikiSearchPage;
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Wiki_Step_Definitions {
+
+WikiSearchPage wikiSearchPage=new WikiSearchPage();
+
+
+}
+
